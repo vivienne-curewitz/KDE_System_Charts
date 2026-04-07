7a6e791480987d9ec1a29baee7b8dfa8ce73594c
vivienne <jaycie.curewitz@gmail.com>
2026-04-07 21:14:24 +0200

    main for mem testing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 mod cpu_reader;
+mod mem_reader;
 
 fn main() {
     println!("Starting CPU Proc Daemon");
-    let mut procman = cpu_reader::ProcFileManager::new();
-    procman.read_and_print_circle_buffer(8, 20);
+    // let mut procman = cpu_reader::ProcFileManager::new();
+    // procman.read_and_print_circle_buffer(8, 20);
+    let mut procmem = mem_reader::MemInfo::new();
+    loop {
+        procmem.read_mem_file();
+        procmem.print_data();
+    }
 }
